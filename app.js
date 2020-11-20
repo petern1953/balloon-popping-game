63dda68ea353142ef2363e7dafd57a5b1fade09e
petern1953 <petern1953@gmail.com>
2020-11-20 22:01:29 +0100

    ready

--- app.js
+++ app.js
@@ -4,11 +4,20 @@ const balloons = document.querySelectorAll('.balloon');
 const endText = document.querySelector('#say-no-balloons');
 
 function popBalloon() {
-    this.setAttribute('class', 'inVisible');
-    console.log(this.getAttribute('class'));
-    if ((popCounter += 1) == 25) endText.setAttribute('style', 'display: inline');
+    // this.setAttribute('class', 'growing');
+    this.setAttribute('style', 'background-color: #ededed');
+    this.removeEventListener('mouseover', popBalloon)
+    setTimeout(() => this.setAttribute('class', 'inVisible'), 400);
+    // console.log(this.getAttribute('class'));
+    console.log(popCounter);
+    if ((popCounter += 1) > 24) setTimeout(() => endText.setAttribute('style', 'display: inline'), 300);
 }
+
+// function hideBalloon() {
+//     setTimeout(() => this.setAttribute('class', 'inVisible'), 300);
+// }
 
 (function setBalloonsPopEvent() {
     balloons.forEach(balloon => balloon.addEventListener('mouseover', popBalloon));
+    // balloons.forEach(balloon => balloon.addEventListener('mouseout', hideBalloon));
 })()
